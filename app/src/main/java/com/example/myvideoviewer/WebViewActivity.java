132777e9c2d4f1e7e8b203db95efad453b9776e4
forComport <drdntstop@gmail.com>
2021-10-26 22:12:15 +0900

    save webview url

--- app/src/main/java/com/example/myvideoviewer/WebViewActivity.java
+++ app/src/main/java/com/example/myvideoviewer/WebViewActivity.java
@@ -1,22 +1,8 @@
 package com.example.myvideoviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.MenuItemCompat;
 
-import android.app.SearchManager;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
-import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,97 +10,42 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.SearchView;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.example.myvideoviewer.contents.DetailActivity;
 import com.example.myvideoviewer.contents.VRController;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import java.util.ArrayList;
-import java.util.UUID;
 
 public class WebViewActivity extends AppCompatActivity implements VRController.Listener {
 
     private static final String TAG = "WebViewActivityTAG";
-    private static final String BOOK_MARK = "BookMark";
-    private static final String PREF_NAME = "WebViewActivity";
+    private static final String PrefName = "WebViewActivity";
+    private static final String KEY_LAST_URL = "last_url";
 
     private VRController vrCtrl;
-    private Menu optionMenu;
-    private SearchView mSearchView;
     private WebView mWebView;
-    private ArrayAdapter mAdapter;
-    private ArrayList<String> mSpinner = new ArrayList<>();
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
-        optionMenu = menu;
-
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        mSearchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        mSearchView.setIconified(false);
-        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-
-        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                mWebView.loadUrl(query);
-                menu.findItem(R.id.search).collapseActionView();
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-        SharedPreferences pref = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        mSpinner = toArray(pref.getString(BOOK_MARK, null));
-        mAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, mSpinner);
-        MenuItem item = menu.findItem(R.id.favorite);
-        Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
-        spinner.setAdapter(mAdapter);
-        spinner.setSelection(0, false);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("WebViewActivityTag", "onItemSelected " + position);
-                if (position == 0) {
-                    String url = mWebView.getUrl();
-                    mSpinner.add(url);
-                    SharedPreferences pref = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-                    pref.edit().putString(BOOK_MARK, toJson(mSpinner)).commit();
-                    mAdapter.clear();
-                    mAdapter.addAll(mSpinner);
-                    mAdapter.notifyDataSetChanged();
-                } else {
-                    String url = mSpinner.get(position);
-                    mWebView.loadUrl(url);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Log.d("WebViewActivityTag", "onNothingSelected");
-            }
-        });
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.favorite) {
+            SharedPreferences pref = getSharedPreferences(PrefName, Context.MODE_PRIVATE);
+            pref.edit().putString(KEY_LAST_URL, mWebView.getUrl()).commit();
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -129,8 +60,14 @@ public class WebViewActivity extends AppCompatActivity implements VRController.L
         WebSettings settings = mWebView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
-//        mWebView.loadUrl("https://google.co.kr");
-        mWebView.loadUrl("https://blacktoon130.com");
+
+        SharedPreferences pref = getSharedPreferences(PrefName, Context.MODE_PRIVATE);
+        String url = pref.getString(KEY_LAST_URL, null);
+        if(url == null) {
+            mWebView.loadUrl("https://google.co.kr");
+        } else {
+            mWebView.loadUrl(url);
+        }
 
         vrCtrl = new VRController(this);
         vrCtrl.setListener(this);
@@ -150,30 +87,6 @@ public class WebViewActivity extends AppCompatActivity implements VRController.L
     protected void onDestroy() {
         vrCtrl.destroy();
         super.onDestroy();
-    }
-
-    private String toJson(ArrayList<String> arr){
-        JSONArray json = new JSONArray();
-        for(String item : arr) {
-            json.put(item);
-        }
-        return json.toString();
-    }
-
-    private ArrayList toArray(String json) {
-        ArrayList<String> arr = new ArrayList<>();
-        if (json == null) {
-            arr.add("추가");
-        } else {
-            try {
-                JSONArray array = new JSONArray(json);
-                for(int i=0;i<array.length();i++) {
-                    arr.add(array.getString(i));
-                }
-            } catch (JSONException e) {}
-        }
-        Log.d("WebViewActivityTag", arr.toString());
-        return arr;
     }
 
     @Override
